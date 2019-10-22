09d7d4cc03075753052293788e13afc7bdc2d194
Sergey Alekseev <srg.serg2013@gmail.com>
2019-10-22 21:13:53 +0300

    firstCommit

---
+++ app/src/main/java/bonch/dev/school/ui/activities/SignInActivity.kt
@@ -0,0 +1,9 @@
+package bonch.dev.school.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.school.R
+
+
+class SignInActivity : AppCompatActivity(){
+}
---
+++ app/src/main/java/bonch/dev/school/ui/activities/SignUpActivity.kt
@@ -0,0 +1,8 @@
+package bonch.dev.school.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.school.R
+
+class SignUpActivity : AppCompatActivity() {
+}
